bec25d349567f6bd3c4612e241c5e8d67d9218bc
David Espino <davidespinog@gmail.com>
2024-06-12 15:31:35 -0600

    Add Apollo Context

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
 const connectDB = require('./config/db');
+const jwt = require('jsonwebtoken');
 
 connectDB();
 
 const server = new ApolloServer({
     typeDefs, 
-    resolvers
+    resolvers,
+    context: ({req}) => {
+        const token = req.headers['authorization'];
+        try {
+            const user = jwt.verify(token, process.env.JWT_SECRET);
+            return { user };
+        } catch(e) {
+            console.log(e);
+        }
+    }
 });
-
-
 
 server.listen().then(({url}) => {
     console.log(`Server running in ${url}`)
