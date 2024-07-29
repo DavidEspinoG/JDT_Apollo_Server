4ca8b1e4d88bf914cbf5ee94992c066575770a92
David Espino <davidespinog@gmail.com>
2024-07-29 15:07:15 -0600

    Add newOrder mutation

--- resolvers/orderResolver.js
+++ resolvers/orderResolver.js
@@ -1,6 +1,8 @@
 const { Query } = require("./productResolver");
 const verifyUserExistsAndIsAuthorized = require("./utils/verifyUserExistAndIsAuthorized");
 const Client = require('../models/client');
+const Product = require('../models/product');
+const Order = require('../models/order');
 
 const orderResolver = {
     Query: {
@@ -9,21 +11,27 @@ const orderResolver = {
     Mutation: {
         newOrder: async (_, {data}, ctx) => {
             // verify client exists and is authorized
-            const { client:clientId } = data;    
+            const { client:clientId, products } = data;    
             let client = await Client.findById(clientId);
             verifyUserExistsAndIsAuthorized(client, ctx);
-            return {
-                total: 200.0
-            }
-
-
             // Check stock
+            for await (const product of products) {
+                const stockProduct = await Product.findById(product?.id);
+                if(product.quantity > stockProduct.items){
+                    throw new Error(`Not enough stock`)
+                } else {
+                    stockProduct.items = stockProduct.items - product.quantity
+                    await stockProduct.save();
+                }
 
-
+            }
+            // create an order 
+            const newOrder = new Order(data);
             // asign a seller 
-
-
+            newOrder.seller = ctx.user.id;
             // Save it in DB
+            const res = await newOrder.save();
+            return res; 
         }
     }
 };
